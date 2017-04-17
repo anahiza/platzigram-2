3cd34a9398e7b55b9a4e28b912891c73dcad18ae
Anhy <anahi.z.a@gmail.com>
2017-04-17 14:39:47 -0500

    Agregando navegación con Page.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert('hola')
+var page = require('page');
+var main = document.getElementById('main-container')
+
+page('/', function(ctx, next){
+  main.innerHTML='home'
+})
+
+page('/signup', function(ctx, next){
+  main.innerHTML='signup'
+})
+
+page.start()
