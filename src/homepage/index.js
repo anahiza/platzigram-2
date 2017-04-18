d2b5478a76ca2798da995a8c3150835a0e45b207
Anhy <anahi.z.a@gmail.com>
2017-04-18 15:11:18 -0500

    Agregando imágenes de los usuarios al timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -7,6 +7,31 @@ var template = require('./template')
 page('/', function(ctx, next){
   title= 'Platzigram'
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template)
+  var pictures=[
+    {
+      user:{
+        username:'Anahi',
+        avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
+      },
+      url:'https://images.pexels.com/photos/214221/pexels-photo-214221.jpeg',
+      likes:0,
+      liked: false,
+      createdAt: +new Date()
+    },
+    {
+      user:{
+        username:'Anahi',
+        avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
+      },
+      url:'https://image.freepik.com/free-photo/working-with-a-coffee_1112-145.jpg',
+      likes:156,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate()-10)
+      
+    }
+  ]
+
+
+  empty(main).appendChild(template(pictures))
 
 })
