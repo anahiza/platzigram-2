3700710d6749f5989928e2e883b21521ee309753
Anhy <anahi.z.a@gmail.com>
2017-04-21 13:29:21 -0500

    Obteniendo una respuesta del servidor con Superagent

--- server.js
+++ server.js
@@ -17,6 +17,37 @@ app.get('/signin', function(req, res){
   res.render('index', {title: 'Platzigram - Sign in'})
 })
 
+app.get('/api/pictures', function(req,res){
+
+  var pictures=[
+    {
+      user:{
+        username:'Anahi',
+        avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
+      },
+      url:'https://images.pexels.com/photos/214221/pexels-photo-214221.jpeg',
+      likes:0,
+      liked: false,
+      createdAt: +new Date()
+    },
+    {
+      user:{
+        username:'Anahi',
+        avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
+      },
+      url:'https://image.freepik.com/free-photo/working-with-a-coffee_1112-145.jpg',
+      likes:156,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate()-10)
+      
+    }
+  ]
+  setTimeout(function(){
+    res.send(pictures)
+  },1000)
+
+})
+
 app.listen(3000, function (err) {
   if(err) return console.log(err.message), process.exit(1);
   console.log("Platzigram escuchando en el puerto 3000")
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,35 +3,23 @@ var yo = require('yo-yo')
 var empty = require('empty-element')
 var title = require('title')
 var template = require('./template')
+var request = require('superagent')
+var header = require('../header')
 
-page('/', function(ctx, next){
+page('/', header, loadPictures, function(ctx, next){
   title= 'Platzigram'
-  var main = document.getElementById('main-container');
-  var pictures=[
-    {
-      user:{
-        username:'Anahi',
-        avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
-      },
-      url:'https://images.pexels.com/photos/214221/pexels-photo-214221.jpeg',
-      likes:0,
-      liked: false,
-      createdAt: +new Date()
-    },
-    {
-      user:{
-        username:'Anahi',
-        avatar:'https://lh3.googleusercontent.com/-vHUbokKYy8E/AAAAAAAAAAI/AAAAAAAACIo/xcEEOpJnzfI/s60-p-rw-no/photo.jpg'
-      },
-      url:'https://image.freepik.com/free-photo/working-with-a-coffee_1112-145.jpg',
-      likes:156,
-      liked: true,
-      createdAt: new Date().setDate(new Date().getDate()-10)
-      
-    }
-  ]
+  var main = document.getElementById('main-container');  
 
-
-  empty(main).appendChild(template(pictures))
+  empty(main).appendChild(template(ctx.pictures))
 
 })
+
+function loadPictures(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function(err, res){
+      if (err) return console.log(err) 
+      ctx.pictures = res.body
+      next()
+    })
+}
